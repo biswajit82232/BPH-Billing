9a82350ebd54046cea257b2ccf4330cea34301ab
Biswajit Power Hub <biswajitpowerhub@gmail.com>
2025-11-26 14:12:32 +0530

    chore: remove historical report docs

--- src/components/SignaturePad.jsx
+++ src/components/SignaturePad.jsx
@@ -142,7 +142,7 @@ export default function SignaturePad({ value, onChange, label = 'Signature', wid
               Clear
             </button>
           )}
-        </div>
+          </div>
       </div>
       
       {isMobile && (
